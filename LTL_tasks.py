08c1147dce48b324e9ca822442b3a198a33c83cb
theshadow2030 <hazem030500@gmail.com>
2025-07-01 11:55:38 +0000

    new results

--- LTL_tasks.py
+++ LTL_tasks.py
@@ -7,7 +7,7 @@ items = ['pickaxe', 'lava', 'door', 'gem', 'empty' ]
 #formulas.append(("(F c0) & (F c1) & (F c2)", 3, "task2: visit({0}, {1}, {2})".format(*items)))
 #formulas.append(("F(c0 & F(c1))", 2, "task3: seq_visit({0}, {1})".format(*items)))
 #formulas.append(("F(c0 & F(c1)) & (F c2)", 3, "task5: seq_visit({0}, {1}) + visit({2})".format(*items)))
-#formulas.append(("(F c0) & (F c1) & (G (! c2))", 3, "task7: visit({0}, {1}) + glob_av({2})".format(*items)))
+formulas.append(("(F c0) & (F c1) & (G (! c2))", 3, "task7: visit({0}, {1}) + glob_av({2})".format(*items)))
 formulas.append(("(F c0) & (F c1) & (G (! c2)) & (G(! c3))", 4, "task8: visit({0}, {1}) + glob_av({2}) + glob_av({3})".format(*items)))
 formulas.append(("F(c0 & F(c1)) & G (! c2)", 3, "task9: seq_visit({0}, {1}) + glob_av({2})".format(*items)))
 formulas.append(("F(c0 & F(c1)) & G (! c2) & G(! c3)", 4, "task10: seq_visit({0}, {1}) + glob_av({2}) + glob_av({3})".format(*items)))
@@ -26,5 +26,4 @@ Task 5 -> Task 7
 Task 6 -> Task 8
 Task 7 -> Task 9
 Task 8 -> Task 10
-
 """
